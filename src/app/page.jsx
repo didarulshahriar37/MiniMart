50ef7b95f3da69c35d370cf82223c6127ae8f03d
Didarul Shahriar <didarul13579@gmail.com>
2026-01-16 22:38:26 +0600

    added Featured Items and Available Categories section headlines

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 import About from "@/components/About/About";
+import AvailableCategories from "@/components/Available Categories/AvailableCategories";
 import FAQ from "@/components/FAQ/FAQ";
+import FeaturedItems from "@/components/Featured Items/FeaturedItems";
 import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
@@ -15,6 +17,8 @@ export default function Home() {
       <main className="w-11/12 mx-auto min-h-screen">
         <Hero></Hero>
         <About></About>
+        <FeaturedItems></FeaturedItems>
+        <AvailableCategories></AvailableCategories>
         <WhyChooseUs></WhyChooseUs>
         <FAQ></FAQ>
         <NewsLetter></NewsLetter>
--- src/components/Available Categories/AvailableCategories.jsx
+++ src/components/Available Categories/AvailableCategories.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function FeaturedItems() {
+export default function AvailableCategories() {
   return (
     <div>
         <h2 className="mt-15 md:mt-40 text-center md:text-4xl text-2xl font-bold mb-5">
-          Featured Items
+          Available Categories
         </h2>
     </div>
   )
